d0870486985293e11890ceab5308c04395447ae3
Joel DEROCHE <jederaliz@gmail.com>
2016-10-23 12:27:27 +0200

    Test le Drag Drop pour d?placment de fen?tre

--- Assets/Orkad/Scripts/AnnimatedUI.cs
+++ Assets/Orkad/Scripts/AnnimatedUI.cs
@@ -1,32 +1,62 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 
-public abstract class AnnimatedUI : MonoBehaviour {
-	public bool show = false;
-	public float transitionDuration = 0.2f;
+public abstract class AnnimatedUI : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+  public bool show = false;
+  public float transitionDuration = 0.2f;
 
-	public void Show(){
-		show = true;
-		OnShow.Invoke();
-	}
+  public void Show()
+  {
+    show = true;
+    OnShow.Invoke();
+  }
 
-	public void Hide(){
-		show = false;
-		OnHide.Invoke();
-	}
+  public void Hide()
+  {
+    show = false;
+    OnHide.Invoke();
+  }
 
-	public void Toogle(){
-		if(IsShown())
-			Hide();
-		else
-			Show();
-	}
+  public void Toogle()
+  {
+    if (IsShown())
+      Hide();
+    else
+      Show();
+  }
 
-	public UnityEvent OnShow;
-	public UnityEvent OnHide;
+  public UnityEvent OnShow;
+  public UnityEvent OnHide;
 
-	public abstract bool IsShown();
-	public abstract bool IsHidden();
+  public abstract bool IsShown();
+  public abstract bool IsHidden();
+
+  public void OnBeginDrag(PointerEventData eventData)
+  {
+    if (show)
+    {
+      transform.position = eventData.position;
+
+      RectTransform rct = this.GetComponent<RectTransform>();
+      float x = (rct.anchoredPosition.x) - eventData.position.x;
+      float y =  (rct.anchoredPosition.y) - eventData.position.y;
+
+      print(new Vector2(x,y));
+    }
+
+  }
+
+  public void OnDrag(PointerEventData eventData)
+  {
+    if (show)
+    {
+      transform.position = eventData.position;
+      print((Vector2)this.GetComponent<RectTransform>().anchoredPosition- eventData.position);
+    }
+  }
+
 
 }
